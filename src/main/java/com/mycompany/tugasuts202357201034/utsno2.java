7c0ea62409adae58de181b065c4465ec2b07928f
rifaul <rifaul@LAPTOP-T5SUOBK1>
2024-05-16 00:44:31 +0700

    first commit

--- src/main/java/com/mycompany/tugasuts202357201034/utsno2.java
+++ src/main/java/com/mycompany/tugasuts202357201034/utsno2.java
@@ -10,16 +10,14 @@ package com.mycompany.tugasuts202357201034;
  */
 public class utsno2 {
 
-    public class MissingLetterFinder {
-
         public static char findMissingLetter(char[] array) {
             for (int i = 0; i < array.length - 1; i++) {
                 if (array[i + 1] - array[i] > 1) {
                     return (char) (array[i] + 1);
                 }
             }
-            
-        }return ' ';
+            return ' ';
+        }
 
         public static void main(String[] args) {
             char[] array1 = {'a', 'b', 'c', 'd', 'f'};
@@ -29,5 +27,3 @@ public class utsno2 {
             System.out.println(findMissingLetter(array2));
         }
     }
-
-}
